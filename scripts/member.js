3b4821f18f48fdb91322cb19639dcb918007856c
923ta <takafumi923wan@gmail.com>
2022-03-28 16:42:17 +0900

    .is-active,.is-showについての問題の解決

--- scripts/member.js
+++ scripts/member.js
@@ -1,3 +1,24 @@
-document.getElementsByClassName('is-active')[0].addEventListener('click', function(){
-  this.classList.toggle('is-active');
-}, false);
+const tabContainer = document.getElementsByClassName('tab');
+const targetContainer = document.getElementsByClassName('target');
+ 
+for (let i = 0; i < tabContainer.length; i++) {
+  tabAction(tabContainer[i], i);
+}
+
+function tabAction(tabDOM, tabId) {
+  tabDOM.addEventListener("click", function(){
+    this.classList.toggle('is-active');
+    targetContainer[tabId].classList.toggle('is-show');
+
+    for (let i = 0; i < tabContainer.length; i++) {
+      if (tabId !== i) {
+        if (tabContainer[i].classList.contains('is-active')){
+          tabContainer[i].classList.remove('is-active');
+        }
+        if (targetContainer[i].classList.contains('is-show')){
+          targetContainer[i].classList.remove('is-show');
+        }
+      }
+    }
+  })
+}
